bc51835a4e3103e96eddc802c18f986b1dd58e8a
nischal shrestha <nischalstha9@gmail.com>
2022-02-14 09:29:52 +0545

    dom man

---
+++ DOM_Manipulation/TEST/app.js
@@ -0,0 +1,28 @@
+// const title = document.getElementById('hero-title');
+// console.log(title);
+// console.dir(title);
+
+// console.log(document.getElementsByClassName('list-item'));
+
+// console.log(document.querySelector('.list-item')); // returns only single/first list item
+
+// console.log(document.querySelectorAll('.list-item'));
+
+// console.log(document.querySelector('ul li:first-of-type'));
+
+// title.textContent = 'welcome';
+
+// title.style.backgroundColor = 'red';
+// title.style.color = 'white';
+
+const list_items = document.querySelectorAll(".list-item")
+
+list_items.forEach(item => { //setting bg color to yellow
+    item.style.backgroundColor = "yellow";
+})
+
+const image = document.querySelector("#img-chair")
+setTimeout(() => {
+    image.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png")
+}, 5000);
+
