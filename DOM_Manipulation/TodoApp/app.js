5ad317fce3deccd4b5d451906159eb7f89e468f5
Nischal Shrestha <nischalstha9@gmail.com>
2022-02-25 14:37:32 +0545

    Styling and LocalStorage Feature

--- DOM_Manipulation/TodoApp/app.js
+++ DOM_Manipulation/TodoApp/app.js
@@ -1,30 +1,31 @@
 let input = document.getElementById("taskNameInput");
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("todo_app_tasks")) || [];
 let form = document.getElementById("taskForm");
 const addTask = (task) => {
-    let id;
-    try {
-        id = tasks[tasks.length - 1].id + 1;
-    } catch {
-        id = 1;
-    }
-    if (task !== "") {
-        tasks.push({ title: task, id: id });
-        refreshTasks();
-        input.value = "";
-        input.focus();
-    }
-}
+  let id;
+  if (tasks.length > 0) {
+    id = tasks[tasks.length - 1].id + 1;
+  } else {
+    id = 1;
+  }
+  if (task !== "") {
+    tasks.push({ title: task, id: id });
+    localStorage.setItem("todo_app_tasks", JSON.stringify(tasks));
+    refreshTasks();
+    input.value = "";
+    input.focus();
+  }
+};
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addTask(input.value);
-})
+  e.preventDefault();
+  addTask(input.value);
+});
 
-const tasksSection = document.querySelector(".tasks-section")
+const tasksSection = document.querySelector(".tasks-section");
 function refreshTasks() {
-    let html = tasks.reverse().map(task => {
-        return `
+  let html = tasks.map((task) => {
+    return `
             <div class="task">
                 <div class="title">
                     <h4>${task.title}</h4>
@@ -33,14 +34,16 @@ function refreshTasks() {
                     <button class="btn" onclick="delItem(${task.id})">Delete</button>
                 </div>
             </div>
-        `
-    })
-    tasksSection.innerHTML = html.join("")
+        `;
+  });
+  tasksSection.innerHTML = html.join("");
 }
 
 function delItem(id) {
-    tasks = tasks.filter(function (task) {
-        return parseInt(id) !== parseInt(task.id);
-    })
-    refreshTasks();
+  tasks = tasks.filter(function (task) {
+    return parseInt(id) !== parseInt(task.id);
+  });
+  localStorage.setItem("todo_app_tasks", JSON.stringify(tasks));
+  refreshTasks();
 }
+refreshTasks();
