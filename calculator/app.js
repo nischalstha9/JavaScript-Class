b07a3a109717a385190f7207fe428071f82ef1ac
Nischal Shrestha <nischalstha9@gmail.com>
2022-03-22 14:18:34 +0545

    calculator syntax err

--- calculator/app.js
+++ calculator/app.js
@@ -1,23 +1,26 @@
-const funcButtons = document.querySelectorAll(".calc-btns")
-let evalStack = ""
-const output = document.querySelector(".display")
-const operationalBtns = ["=", "AC", "DEL"]
+const funcButtons = document.querySelectorAll(".calc-btns");
+let evalStack = "";
+const output = document.querySelector(".display");
+const operationalBtns = ["=", "AC", "DEL"];
 
-funcButtons.forEach(btn => {
-    let btnValue = btn.innerText;
-    btn.addEventListener("click", (e) => {
-        if (!operationalBtns.includes(btnValue)) {
-            evalStack += btnValue;
-            output.innerText = evalStack;
-        } else if (btnValue == "=") {
-            output.innerText = eval(evalStack);
-        } else if (btnValue == "DEL") {
-            evalStack = evalStack.slice(0, -1)
-            output.innerText = evalStack;
-        }
-        else {
-            evalStack = ""
-            output.innerText = evalStack;
-        }
-    })
-})
+funcButtons.forEach((btn) => {
+  let btnValue = btn.innerText;
+  btn.addEventListener("click", (e) => {
+    if (!operationalBtns.includes(btnValue)) {
+      evalStack += btnValue;
+      output.innerText = evalStack;
+    } else if (btnValue == "=") {
+      try {
+        output.innerText = eval(evalStack);
+      } catch {
+        output.innerText = "SYNTAX ERR";
+      }
+    } else if (btnValue == "DEL") {
+      evalStack = evalStack.slice(0, -1);
+      output.innerText = evalStack;
+    } else {
+      evalStack = "";
+      output.innerText = evalStack;
+    }
+  });
+});
